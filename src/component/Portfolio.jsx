f66d950ece36a139cdd619984e7073a1a1dcdc9b
VSerra12 <vicky.serra.24@gmail.com>
2024-08-06 19:29:35 -0300

    1

--- src/component/Portfolio.jsx
+++ src/component/Portfolio.jsx
@@ -1,8 +1,10 @@
+
 import linkedInLogo from '../assets/linkedin.svg'
 import gitHubLogo from '../assets/github.svg'
 import emailLogo from '../assets/envelope-at.svg'
 import chevronLeft from '../assets/chevron-left.svg'
 import { useState } from 'react';
+import { Fade } from 'react-bootstrap';
 
 
 const Portfolio = () => {
@@ -15,6 +17,18 @@ const Portfolio = () => {
 
         setSelected(e.target.id)
     }
+
+    const isSelected = (num) => {
+        return selected === num
+    }
+
+    // tabs.click(function() {
+    //     var content = this.hash.replace('/','');
+    //     tabs.removeClass("active");
+    //     $(this).addClass("active");
+    //     $("#content").find('p').hide();
+    //     $(content).fadeIn(200);
+    //   });
 
     return (
         <>
@@ -43,17 +57,17 @@ const Portfolio = () => {
                         <div className="tabs">
                             <div className='tab-border'>
                                 <div className="tab-header">
-                                    <div id='0' className={`${selected === "0" ? 'active' : ''}`} onClick={selectedTab}>
-                                        <i className="fa fa-code"></i> Code
+                                    <div id='0' className={`${isSelected("0")? 'active' : ''}`} onClick={selectedTab}>
+                                        Code
                                     </div>
-                                    <div id='1' className={`${selected === "1" ? 'active' : ''}`} onClick={selectedTab}>
-                                        <i className="fa fa-pencil-square"></i> About
+                                    <div id='1' className={`${isSelected("1")? 'active' : ''}`} onClick={selectedTab}>
+                                        About
                                     </div>
-                                    <div id='2' className={`${selected === "2" ? 'active' : ''}`} onClick={selectedTab}>
-                                        <i className="fa fa-bar-chart"></i> Services
+                                    <div id='2' className={`${isSelected("2")? 'active' : ''}`} onClick={selectedTab}>
+                                        Services
                                     </div>
-                                    <div id='3' className={`${selected === "3" ? 'active' : ''}`} onClick={selectedTab}>
-                                        <i className="fa fa-envelope"></i> Contact
+                                    <div id='3' className={`${isSelected("3")? 'active' : ''}`} onClick={selectedTab}>
+                                        Contact
                                     </div>
                                 </div>
                                 <div className="tab-indicator" style={{ marginTop: `calc(50px + ${selected * 50}px)` }}><img src={chevronLeft} alt="Logo" className="d-inline-block align-text-top shadow-svg"></img></div>
@@ -62,30 +76,30 @@ const Portfolio = () => {
                     </div>
                     <div className="w-50 p-3">
                         <div className="tab-content">
-
-                            <div className={`${selected === "0" ? 'active' : ''}`}>
-                                <i className="fa fa-code"></i>
-                                <h2>This is code section</h2>
-                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis eum similique quisquam officiis neque, cumque dignissimos architecto nisi totam sapiente eos et illum laborum atque vero ea perferendis consectetur veritatis.</p>
-                            </div>
-
-                            <div className={`${selected === "1" ? 'active' : ''}`}>
-                                <i className="fa fa-pencil-square"></i>
-                                <h2>This is about section</h2>
-                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis eum similique quisquam officiis neque, cumque dignissimos architecto nisi totam sapiente eos et illum laborum atque vero ea perferendis consectetur veritatis.</p>
-                            </div>
-
-                            <div className={`${selected === "2" ? 'active' : ''}`}>
-                                <i className="fa fa-bar-chart"></i>
-                                <h2>This is services section</h2>
-                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis eum similique quisquam officiis neque, cumque dignissimos architecto nisi totam sapiente eos et illum laborum atque vero ea perferendis consectetur veritatis.</p>
-                            </div>
-
-                            <div className={`${selected === "3" ? 'active' : ''}`}>
-                                <i className="fa fa-envelope"></i>
-                                <h2>This is contact section</h2>
-                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis eum similique quisquam officiis neque, cumque dignissimos architecto nisi totam sapiente eos et illum laborum atque vero ea perferendis consectetur veritatis.</p>
-                            </div>
+                            <Fade in={isSelected("0")} className={`${isSelected("0")? '' : 'tab'}`}>
+                                <div>
+                                    <h2>This is code section</h2>
+                                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis eum similique quisquam officiis neque, cumque dignissimos architecto nisi totam sapiente eos et illum laborum atque vero ea perferendis consectetur veritatis.</p>
+                                </div>
+                            </Fade>
+                            <Fade in={isSelected("1")} className={`${isSelected("1")? '' : 'tab'}`}>
+                                <div>
+                                    <h2>This is about section</h2>
+                                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis eum similique quisquam officiis neque, cumque dignissimos architecto nisi totam sapiente eos et illum laborum atque vero ea perferendis consectetur veritatis.</p>
+                                </div>
+                            </Fade>
+                            <Fade in={isSelected("2")} className={`${isSelected("2")? '' : 'tab'}`}>
+                                <div>
+                                    <h2>This is services section</h2>
+                                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis eum similique quisquam officiis neque, cumque dignissimos architecto nisi totam sapiente eos et illum laborum atque vero ea perferendis consectetur veritatis.</p>
+                                </div>
+                            </Fade>
+                            <Fade in={isSelected("3")} className={`${isSelected("3")? '' : 'tab'}`}>
+                                <div>
+                                    <h2>This is contact section</h2>
+                                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis eum similique quisquam officiis neque, cumque dignissimos architecto nisi totam sapiente eos et illum laborum atque vero ea perferendis consectetur veritatis.</p>
+                                </div>
+                            </Fade>
                         </div>
                     </div>
                 </div>
