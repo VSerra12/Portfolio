95f9ec0edd2ac8a457c9752504be7f29277c3ab1
VSerra12 <vicky.serra.24@gmail.com>
2024-08-08 19:12:18 -0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 
+import React from 'react'
 import './App.css'
 import Portfolio from './component/Portfolio'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <Portfolio/>
+      <React.StrictMode>
+        <Routes>
+          <Route path='/Portfolio/' element={<Portfolio/>}/>
+        </Routes>
+      </React.StrictMode>,
     </>
   )
 }
